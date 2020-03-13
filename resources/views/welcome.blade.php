990ef1d09f58f62fa1ced5d25210ddb27670da84
ilijan.militar <ilijan.militar@quantox.com>
2020-03-13 19:52:48 +0100

    change title on first page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Web site checker
                 </div>
 
                 <div class="links">
